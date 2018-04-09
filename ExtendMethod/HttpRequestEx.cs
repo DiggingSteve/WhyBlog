c269e9c855bcba5197b75541ebdad332a298b9ed
Steve <doublefly@live.cn>
2018-04-09 10:45:13 +0800

    add extensions httprequest

---
+++ ExtendMethod/HttpRequestEx.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Text;
+
+namespace ExtendMethod
+{
+    public static class HttpRequestEx
+    {
+        public static string GetAbsoluteUri(this HttpRequest request)
+        {
+            return new StringBuilder()
+                .Append(request.Scheme)
+                .Append("://")
+                .Append(request.Host)
+                .Append(request.PathBase)
+                .Append(request.Path)
+                .Append(request.QueryString)
+                .ToString();
+        }
+    }
+}
