31088c4154c0e51cad1a47129f857212b6881a80
Steve <doublefly@live.cn>
2018-04-17 10:48:38 +0800

    change  principal method from linq to lambda

--- WhyBlog.Infrastructure/CookieUtil.cs
+++ WhyBlog.Infrastructure/CookieUtil.cs
@@ -6,13 +6,15 @@ using System.Text;
 
 namespace WhyBlog.Infrastructure
 {
-  public   class CookieUtil
+    public class CookieUtil
     {
         public static string GetCookie(string key, ClaimsPrincipal principal)
         {
-           return (from c in principal.Claims
-                          where c.Type == key
-                          select c.Value).FirstOrDefault();
+            //return (from c in principal.Claims
+            //               where c.Type == key
+            //               select c.Value).FirstOrDefault();
+
+            return principal.Claims.Where(o => o.Type == key).Select(o => o.Value).FirstOrDefault();
         }
     }
 }
